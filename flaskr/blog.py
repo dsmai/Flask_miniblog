80cee94c34e4a183a9362bdd4ace51d757084321
Son Mai <son.dmai@gmail.com>
2023-10-01 23:58:42 -0400

    Added the delete view. Completing basic functions of blog app.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -88,3 +88,14 @@ def update(id):
             return redirect(url_for("blog.index"))
 
     return render_template("blog/update.html", post=post)
+
+
+# this view/endpoint deletes a blog post
+@bp.route("/<int:id>/delete", methods=("POST",))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute("DELETE FROM post WHERE id = ?", (id,))
+    db.commit()
+    return redirect(url_for("blog.index"))
